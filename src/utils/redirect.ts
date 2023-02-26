ce5091250e455bae6f6369bb2888dac8a3ecef15
maksymivashchenko <mivashchenko@react.vslabs.com>
2023-02-26 21:16:06 +0100

    Updated contact redirect status page

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -10,7 +10,7 @@ import {
 export const redirectToYoutube = () => window.open(youtube);
 export const redirectToInstagram = () => window.open(instagram);
 export const redirectToTelegramChannel = () => window.open(telegramChannel);
-export const redirectToPhone = () => window.open(`tel:${phoneNumber}`);
-export const redirectToEmail = () => window.open(`mailto:${email}`);
+export const redirectToPhone = () => window.open(`tel:${phoneNumber}`, '_self');
+export const redirectToEmail = () => window.open(`mailto:${email}`, '_self');
 export const redirectToTelegramPrivateLink = () =>
   window.open(telegramPrivateLink);
